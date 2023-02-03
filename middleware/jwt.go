22542618e5db449755fbc096c31f1f1933705e74
zcwhy <zcwhy@github.com>
2023-02-03 09:45:07 +0800

    add jwt checkId middleware

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,8 +1,11 @@
 package middleware
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 	"miniDy/model"
+	"miniDy/model/response"
+	"net/http"
 	"time"
 )
 
@@ -29,4 +32,61 @@ func ReleaseToken(user *model.UserLogin) (string, error) {
 		return "", err
 	}
 	return tokenString, nil
+
+}
+
+func ParseToken(tokenString string) (*Claims, bool) {
+	token, _ := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if token != nil {
+		//
+		if claims, ok := token.Claims.(*Claims); ok {
+			if token.Valid {
+				return claims, true
+			} else {
+				return claims, false
+			}
+		}
+	}
+	return nil, false
+}
+
+func JWTMiddleWare(c *gin.Context) {
+	token := c.Query("token")
+
+	if token == "" {
+		token = c.PostForm("token")
+	}
+
+	//用户不存在
+	if token == "" {
+		c.JSON(http.StatusOK, response.CommonResp{StatusCode: 401, StatusMsg: "用户不存在"})
+		c.Abort() //阻止执行
+		return
+	}
+
+	//验证token
+	claims, ok := ParseToken(token)
+	if !ok {
+		c.JSON(http.StatusOK, response.CommonResp{
+			StatusCode: 403,
+			StatusMsg:  "token不正确",
+		})
+		c.Abort() //阻止执行
+		return
+	}
+
+	if time.Now().Unix() > claims.ExpiresAt.Time.Unix() {
+		c.JSON(http.StatusOK, response.CommonResp{
+			StatusCode: 402,
+			StatusMsg:  "token过期",
+		})
+		c.Abort() //阻止执行
+		return
+	}
+
+	c.Set("user_id", claims.UserId)
+	c.Next()
 }
--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"miniDy/handlers/user_login"
+	"miniDy/middleware"
 	"miniDy/model"
 )
 
@@ -10,29 +11,32 @@ func InitRouter() *gin.Engine {
 	model.InitDB()
 
 	r := gin.Default()
+
+	r.Static("/static", "./static")
+
 	baseGroup := r.Group("/douyin")
 
 	//basic apis
 	baseGroup.GET("/feed")
-	baseGroup.GET("/user")
-	baseGroup.GET("/publish/list")
+	baseGroup.GET("/user", middleware.JWTMiddleWare)
+	baseGroup.GET("/publish/list", middleware.CheckIdMiddleWare)
 	baseGroup.POST("/user/login")
 	baseGroup.POST("/user/register", user_login.UserRegisterHandler)
-	baseGroup.POST("/publish/action")
+	baseGroup.POST("/publish/action", middleware.JWTMiddleWare)
 
 	//interaction apis
-	baseGroup.POST("/favorite/action")
-	baseGroup.GET("/favorite/list")
-	baseGroup.POST("/comment/action")
-	baseGroup.GET("/comment/list")
+	baseGroup.POST("/favorite/action", middleware.JWTMiddleWare)
+	baseGroup.GET("/favorite/list", middleware.CheckIdMiddleWare)
+	baseGroup.POST("/comment/action", middleware.JWTMiddleWare)
+	baseGroup.GET("/comment/list", middleware.JWTMiddleWare)
 
 	//social apis
-	baseGroup.POST("/relation/action")
-	baseGroup.GET("/relation/follow/list")
-	baseGroup.GET("/favorite/follower/list")
-	baseGroup.GET("/favorite/friend/list")
-	baseGroup.GET("/favorite/message/chat")
-	baseGroup.GET("/favorite/message/action")
+	baseGroup.POST("/relation/action", middleware.JWTMiddleWare)
+	baseGroup.GET("/relation/follow/list", middleware.CheckIdMiddleWare)
+	baseGroup.GET("/favorite/follower/list", middleware.CheckIdMiddleWare)
+	baseGroup.GET("/favorite/friend/list", middleware.CheckIdMiddleWare)
+	//baseGroup.GET("/favorite/message/chat")
+	//baseGroup.GET("/favorite/message/action")
 
 	return r
 }
